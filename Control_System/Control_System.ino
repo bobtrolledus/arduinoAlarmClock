89cf816a551fc7f5bcd07ca6a246647aa59c5694
bobtrolledus <335730800@gapps.yrdsb.ca>
2022-06-08 09:54:54 -0400

    Update

--- Control_System/Control_System.ino
+++ Control_System/Control_System.ino
@@ -191,17 +191,3 @@ void colours(int values, int wait)
     delay(wait);
   }
 }
-
-uint32_t Wheel(byte WheelPos)
-{
-  WheelPos = 255 - WheelPos;
-  if (WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if (WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
